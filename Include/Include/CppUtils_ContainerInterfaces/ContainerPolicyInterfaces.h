580b11c2da56fe58bfecffaaea88a02a6bc9b878
b2hinkle <b2hinkle@gmail.com>
2026-01-04 04:53:47 -0500

    [cpp] Separate out container policy specialization getter into its own file for organization.

--- Include/Include/CppUtils_ContainerInterfaces/ContainerPolicyInterfaces.h
+++ Include/Include/CppUtils_ContainerInterfaces/ContainerPolicyInterfaces.h
@@ -2,27 +2,7 @@
 
 #pragma once
 
-#include <type_traits>
-#include <CppUtils_ContainerInterfaces/ContainerPolicies_PrimaryTemplate.h>
-
-/*
-* Accessing a container policy specialization. This removes cv and ref qualifiers in the process for compatability with the specializations.
-* For this reason we go through here for the specialization types rather then directly.
-*/
-namespace CppUtils
-{
-    template <class T>
-    struct GetSpecializationForContainerPolicy;
-
-    template <template <class> class TPolicy, class TContainer>
-    struct GetSpecializationForContainerPolicy<TPolicy<TContainer>>
-    {
-        using type = TPolicy<std::remove_cvref_t<TContainer>>;
-    };
-
-    template <class T>
-    using GetSpecializationForContainerPolicy_t = typename GetSpecializationForContainerPolicy<T>::type;
-}
+#include <CppUtils_ContainerInterfaces/ContainerPolicyUtils.h>
 
 /*
 * Static interfaces for container operations which callers go through for generically calling on a container's operation implementation.
