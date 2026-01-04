c3dd6b28b2592fd7c260d63fd6241a6a47dd79f2
b2hinkle <b2hinkle@gmail.com>
2026-01-04 02:10:15 -0500

    [cpp] Create means for accessing policy specializations without need to neuter the container type prior.
    - This also consolidates the neutering process from each policy interface.

--- Include/Include/CppUtils_ContainerInterfaces/ContainerInterfaces.h
+++ Include/Include/CppUtils_ContainerInterfaces/ContainerInterfaces.h
@@ -7,6 +7,25 @@
 #include <CppUtils_ContainerInterfaces/PolicyPrimaryTemplates.h>
 
 /*
+* Accessing a policy specialization. This removes cv and ref qualifiers in the process for compatability with the specializations.
+* For this reason we go through here for the specializations rather then directly.
+*/
+namespace CppUtils
+{
+    template <class T>
+    struct GetSpecializationForPolicy;
+
+    template <template <class> class TPolicy, class TContainer>
+    struct GetSpecializationForPolicy<TPolicy<TContainer>>
+    {
+        using type = TPolicy<std::remove_cvref_t<TContainer>>;
+    };
+
+    template <class T>
+    using GetSpecializationForPolicy_t = typename GetSpecializationForPolicy<T>::type;
+}
+
+/*
 * Static interfaces for container operations which callers go through for generically calling on a container's operation implementation.
 * TODO: Make a non-consteval version to account for dynamically sized arrays.
 * TODO: Potentally also make a pass by value overload, especially helpful for static arrays.
@@ -20,11 +39,9 @@ namespace CppUtils
     struct ContainerPolicyInterface_GetCapacity
     {
         //static_assert(sizeof(T) && std::); // TODO: I want to make enforcements on the doer. It's the whole purpose of this interface type.
-
-        using NeuteredT = std::remove_cvref_t<T>; // TODO: Use mixin approach for inheriting this using to avoid dup code.
-
+        
         // Our implementer which conforms to our enforcements.
-        using Doer = ContainerPolicy_GetCapacity<NeuteredT>;
+        using Doer = GetSpecializationForPolicy_t<ContainerPolicy_GetCapacity<T>>;
     };
 
     /*
@@ -50,10 +67,8 @@ namespace CppUtils
     {
         //static_assert(sizeof(T) && std::); // TODO: I want to make enforcements on the doer. It's the whole purpose of this interface type.
 
-        using NeuteredT = std::remove_cvref_t<T>;
-
         // Our implementer which conforms to our enforcements.
-        using Doer = ContainerPolicy_GetSize<NeuteredT>;
+        using Doer = GetSpecializationForPolicy_t<ContainerPolicy_GetSize<T>>;
     };
     
     template <class T>
@@ -77,10 +92,8 @@ namespace CppUtils
     {
         //static_assert(sizeof(T) && std::); // TODO: I want to make enforcements on the doer. It's the whole purpose of this interface type.
 
-        using NeuteredT = std::remove_cvref_t<T>;
-
         // Our implementer which conforms to our enforcements.
-        using Doer = ContainerPolicy_IsValidIndex<NeuteredT>;
+        using Doer = GetSpecializationForPolicy_t<ContainerPolicy_IsValidIndex<T>>;
     };
     
     // TODO: GENERIC TYPE FOR INDEX!
@@ -108,10 +121,8 @@ namespace CppUtils
     {
         //static_assert(sizeof(T) && std::); // TODO: I want to make enforcements on the doer. It's the whole purpose of this interface type.
 
-        using NeuteredT = std::remove_cvref_t<T>;
-
         // Our implementer which conforms to our enforcements.
-        using Doer = ContainerPolicy_IsEmpty<NeuteredT>;
+        using Doer = GetSpecializationForPolicy_t<ContainerPolicy_IsEmpty<T>>;
     };
     
     template <class T>
@@ -134,10 +145,8 @@ namespace CppUtils
     {
         //static_assert(sizeof(T) && std::); // TODO: I want to make enforcements on the doer. It's the whole purpose of this interface type.
 
-        using NeuteredT = std::remove_cvref_t<T>;
-
         // Our implementer which conforms to our enforcements.
-        using Doer = ContainerPolicy_GetFront<NeuteredT>;
+        using Doer = GetSpecializationForPolicy_t<ContainerPolicy_GetFront<T>>;
     };
     
     template <class T>
@@ -160,10 +169,8 @@ namespace CppUtils
     {
         //static_assert(sizeof(T) && std::); // TODO: I want to make enforcements on the doer. It's the whole purpose of this interface type.
 
-        using NeuteredT = std::remove_cvref_t<T>;
-
         // Our implementer which conforms to our enforcements.
-        using Doer = ContainerPolicy_GetBack<NeuteredT>;
+        using Doer = GetSpecializationForPolicy_t<ContainerPolicy_GetBack<T>>;
     };
     
     template <class T>
@@ -187,10 +194,8 @@ namespace CppUtils
     {
         //static_assert(sizeof(T) && std::); // TODO: I want to make enforcements on the doer. It's the whole purpose of this interface type.
 
-        using NeuteredT = std::remove_cvref_t<T>;
-
         // Our implementer which conforms to our enforcements.
-        using Doer = ContainerPolicy_GetElement<NeuteredT>;
+        using Doer = GetSpecializationForPolicy_t<ContainerPolicy_GetElement<T>>;
     };
     
     // TODO: GENERIC TYPE FOR INDEX!
