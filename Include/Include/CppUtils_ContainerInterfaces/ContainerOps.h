95b34bad5de53f11244c320826875bd7254ffe67
b2hinkle <b2hinkle@gmail.com>
2026-02-02 21:53:04 -0500

    [cpp][documentation] Document container ops, and clean up some unused comments.

--- Include/Include/CppUtils_ContainerInterfaces/ContainerOps.h
+++ Include/Include/CppUtils_ContainerInterfaces/ContainerOps.h
@@ -6,49 +6,37 @@
 #include <CppUtils_ContainerInterfaces/ContainerOpInterfaces.h>
 
 /*
-*
-*/
+ * Container ops provide a uniform, type-erased interface for performing common
+ * container operations without requiring knowledge of the container’s concrete type.
+ *
+ * Each operation is expressed as a lightweight interface wrapper that resolves,
+ * at compile time, to the appropriate container-specific implementation via
+ * specialization. This allows generic code to interact with a wide variety of
+ * container types while retaining static type safety and zero runtime overhead.
+ *
+ * Containers opt into support by providing specializations for the corresponding
+ * ContainerOp_* templates.
+ */
 namespace CppUtils::ContainerOps
 {
-    /*
-    * Gets the capacity.
-    */
     template <class ContainerType>
     using GetCapacity = ContainerOpInterface_GetCapacity<ContainerType>;
 
-    /*
-    * 
-    */
     template <class ContainerType>
     using GetSize = ContainerOpInterface_GetSize<ContainerType>;
 
-    /*
-    * 
-    */
     template <class ContainerType>
     using IsValidIndex = ContainerOpInterface_IsValidIndex<ContainerType>;
 
-    /*
-    * 
-    */
     template <class ContainerType>
     using IsEmpty = ContainerOpInterface_IsEmpty<ContainerType>;
 
-    /*
-    * 
-    */
     template <class ContainerType>
     using GetFront = ContainerOpInterface_GetFront<ContainerType>;
 
-    /*
-    * 
-    */
     template <class ContainerType>
     using GetBack = ContainerOpInterface_GetBack<ContainerType>;
 
-    /*
-    * 
-    */
     template <class ContainerType>
     using GetElement = ContainerOpInterface_GetElement<ContainerType>;
 }
